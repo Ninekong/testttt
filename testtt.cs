888065ed8e93fd9131636b0a73d0c733a80c31db
Ninekong <122553014+Ninekong@users.noreply.github.com>
2023-05-14 22:12:25 +0700

    Add files via upload

---
+++ testtt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LegendaryMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter '+' or '-' (any other character to exit): ");
+                char operation = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (operation != '+' && operation != '-')
+                {
+                    break;
+                }
+
+                Console.Write("Enter the size of the matrix: ");
+                int size = int.Parse(Console.ReadLine());
+
+                double[,] matrix = new double[size, size];
+
+                Console.WriteLine("Enter the matrix values:");
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        matrix[i, j] = double.Parse(Console.ReadLine());
+                    }
+                }
+
+                Console.WriteLine("Result:");
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (operation == '+')
+                        {
+                            Console.Write(Math.Abs(matrix[i, j]) + " ");
+                        }
+                        else
+                        {
+                            Console.Write(-matrix[i, j] + " ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
